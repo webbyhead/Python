bf7eb8f8453bf275ed94e197f02045b45533d77c
webbyhead <thompson.andre@gmail.com>
2020-09-11 11:28:53 -0400

    connect to public Cisco router and queries interfaces

---
+++ xe.py
@@ -0,0 +1,40 @@
+
+# Python script to query Cisco switch running XE IOS
+# Taken from https://www.youtube.com/watch?v=kZ4YqqlU5eM&list=PLhfrWIlLOoKPc2RecyiM_A9nf3fUU3e6g&index=29
+
+
+
+
+
+import requests
+# requests library for REST API queries
+
+import sys
+
+# Allow self signed certs
+requests.packages.urllib3.disable_warnings()
+
+# Credentials
+USER = 'developer'
+PASS = 'C1sco12345'
+
+# URL for GET request - same request for getting the list of interfaces
+url = "https://ios-xe-mgmt.cisco.com:9443/restconf/data/ietf-interfaces:interfaces"
+
+# Set yang+json as the data formats
+# Need both Accept and Content-Type for a POST
+headers = {'Content-Type': 'application/yang-data+json',
+'Accept': 'application/yang-data+json'}
+
+# Run GET
+response = requests.get(url, auth=(USER, PASS),
+			headers=headers, verify=False)
+
+print(response.text)
+
+
+
+
+
+
+
